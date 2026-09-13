099d05fc84233903882ee3716ceb69ee3f08499d
giannidona <gianlucadonato2005@gmail.com>
2026-09-13 01:12:47 -0300

    full products ABM

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -9,3 +9,44 @@ export async function getProducts(req, res) {
     res.status(500).json({ message: error });
   }
 }
+
+export async function getProductById(req, res) {
+  try {
+    const id = req.params.id;
+    const producto = await productService.getProductById(id);
+    if (!producto)
+      return res.status(404).json({ message: "Producto no encontrado" });
+    res.status(200).json(producto);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+export async function saveProduct(req, res) {
+  try {
+    const producto = await productService.saveProduct(req.body);
+    res.status(201).json(producto);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+export async function editProduct(req, res) {
+  try {
+    const id = req.params.id;
+    const producto = await productService.editProduct(id, req.body);
+    res.status(202).json(producto);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
+export async function deleteProduct(req, res) {
+  try {
+    const id = req.params.id;
+    await productService.deleteProduct(id);
+    res.status(202).json({ message: "Producto eliminado" });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
--- api/routes/products.routes.js
+++ api/routes/products.routes.js
@@ -4,5 +4,9 @@ import * as productController from "../controllers/products.controller.js";
 const router = Router();
 
 router.get("/api/products", productController.getProducts);
+router.get("/api/products/:id", productController.getProductById);
+router.post("/api/products", productController.saveProduct);
+router.put("/api/products/:id", productController.editProduct);
+router.delete("/api/products/:id", productController.deleteProduct);
 
 export default router;
