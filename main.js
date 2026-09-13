8f65bdacd1c5f7ebf5b094cccf002ee421c22f8a
giannidona <gianlucadonato2005@gmail.com>
2026-09-13 12:14:26 -0300

    main ready

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import express from "express";
 import productRoutes from "./api/routes/products.routes.js";
 import brandRoutes from "./api/routes/brands.routes.js";
+import categoryRoutes from "./api/routes/categories.routes.js";
 
 const app = express();
 const PORT = 3333;
 
-app.listen(PORT);
-
+app.use(express.json());
 app.use(productRoutes);
 app.use(brandRoutes);
+app.use(categoryRoutes);
+
+app.listen(PORT, () => console.log("Funcionando en el puerto " + PORT));
