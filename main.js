29f6f7d8b4654f524540524a78f29045167c3ae1
giannidona <gianlucadonato2005@gmail.com>
2026-09-12 23:50:09 -0300

    base archives

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import express from "express";
+import db from "./db/connection.js";
 
 const app = express();
 const PORT = 3333;
@@ -8,3 +9,9 @@ app.listen(PORT);
 app.get("/", (req, res) => {
   res.send("¡Hola, mundo!");
 });
+
+async function data() {
+  const data = await db.collection("products").countDocuments();
+  return data;
+}
+console.log(await data());
